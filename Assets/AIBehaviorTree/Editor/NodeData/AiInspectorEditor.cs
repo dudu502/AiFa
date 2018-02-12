30bf7417dcb35cd43f6257f02bade359d8070602
dudu502 <346090036@qq.com>
2018-02-12 11:46:09 +0800

    editor modi

---
+++ Assets/AIBehaviorTree/Editor/NodeData/AiInspectorEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+using AIBehaviorTree;
+
+[CustomEditor(typeof(AI))]
+public class AiInspectorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.BeginVertical("box");
+        base.OnInspectorGUI();
+        EditorGUILayout.EndVertical();
+        var aiScript = target as AIBehaviorTree.AI;
+        var nodes = aiScript.GetExecutingNodes();
+        if (nodes.Count>0)
+        {
+            EditorGUILayout.BeginVertical("box");
+            foreach (var node in nodes)
+            {
+                EditorGUILayout.BeginVertical("box");
+                EditorGUILayout.LabelField("Name", node.Config.name);
+                EditorGUILayout.LabelField("Script", node.Config.scriptName);
+                if (!string.IsNullOrEmpty(node.Config.scriptName))
+                {
+                    if (GUILayout.Button("Edit Script"))
+                    {
+                        Process.Start(Application.dataPath + AI.SCRIPT_OUTPUTPATH + node.Config.scriptName + ".txt");
+                    }
+                }
+                EditorGUILayout.EndVertical();
+            }
+            EditorGUILayout.EndVertical();
+        }
+        if (aiScript.m_JsonAiTree != null && GUILayout.Button("Open NodeEditor"))
+        {        
+            NodeEditor.ShowEditor(aiScript.m_JsonAiTree);          
+        }
+    }
+}
