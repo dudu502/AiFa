0b596e320926880ae1939060278aa936db44bf4d
dudu502 <346090036@qq.com>
2018-06-20 09:20:35 +0800

    mod editor

--- Assets/AIBehaviorTree/Editor/NodeData/NodeEditor.cs
+++ Assets/AIBehaviorTree/Editor/NodeData/NodeEditor.cs
@@ -139,8 +139,9 @@ public class NodeEditor : EditorWindow {
     void InitWindow(NodeGraph parent)
     {
         string title = parent.Parent == null ? "Root" : string.Format("No.{0}", parent.Parent.Nodes.IndexOf(parent));
-        GUI.color = NodeGraph.GetColorByType((int)parent.Type);          
-        parent.NodeRect = GUILayout.Window(parent.ID,parent.NodeRect, DrawNodeWindow, new GUIContent(title),GUILayout.ExpandHeight(true));
+        GUI.color = NodeGraph.GetColorByType((int)parent.Type);
+        parent.NodeRect = new Rect(parent.NodeRect.x, parent.NodeRect.y, parent.NodeRect.width, 200);
+        parent.NodeRect = GUILayout.Window(parent.ID,parent.NodeRect, DrawNodeWindow, new GUIContent(title),GUILayout.ExpandHeight(true),GUILayout.ExpandWidth(true));
         GUI.color = Color.black;
         for (int i = 0; i < parent.Nodes.Count; ++i)
         {
